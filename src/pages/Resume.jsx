c45b9469c70dead0dc428ce16068e17330077268
Cyn-Murillo <cynthia.y.murillo@sjsu.edu>
2025-05-12 13:01:28 -0700

    Corrected resume path

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,9 +3,9 @@ function Resume () {
       <div className="main-content">
         <h1>Resume</h1>
         <p>Click below to view or download my resume:</p>
-        <a
-          href="/CMurillo_Resume.pdf"
-          target="_blank"
+        <a 
+          href="/myPortfolio/CMurillo_Resume.pdf"
+          target="_blank" 
           rel="noopener noreferrer"
           style={{
             color: "#77bbbb",
